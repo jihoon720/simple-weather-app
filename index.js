f749ba3c5e18ce7b3d7993d77d50fd35c1b658af
Ji Hoon Lee <justin@act-analytics.com>
2021-05-06 13:24:40 -0300

    Tweaked few unnecessary lines of code.

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ let request = require('request');
 const argv = require('yargs').argv;
 
 let apikey = 'd3134324c6f7286c52b142a98772f494';
-//default is fredericton, take input from console, flag -c
 let city = argv.c || 'Fredericton';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
 
 request(url, function (err, response, body){
 	if(err)
-	{
 		console.log('error:', error);
-	}
-	else
-	{
+	else {
 		let weather = JSON.parse(body);
-		//console.log('body:', body);
 		console.log(`It's ${weather.main.temp} degrees in ${weather.name}!`);
 	}
 });
